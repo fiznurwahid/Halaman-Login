297d04b2d23749bc5e4139034adfb0341b976760
fiznurwahid <149405781+fiznurwahid@users.noreply.github.com>
2023-11-27 07:19:22 +0700

    Update you.js

--- you.js
+++ you.js
@@ -2,9 +2,9 @@ function validateForm() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username === "HAFIZHNURWAHID" && password === "H@fizh21TI") {
+    if (username === "" && password === "") {
         alert("Login berhasil!");
-        window.location.href = " https://fiznurwahid.github.io/Coding/"; // Ganti URL tujuan sesuai kebutuhan
+        window.location.href = ""; // Ganti URL tujuan sesuai kebutuhan
         return false; // Untuk mencegah pengiriman formulir
     } else {
         alert("Login gagal. Cek kembali username dan password.");
